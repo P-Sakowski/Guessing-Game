d137a3868b2642341c86a259689c1d5372740e7b
Sakowski Przemysław <przemyslaw.sakowski@microsoft.wsei.edu.pl>
2019-05-19 14:32:03 +0200

    Dodano grę obiektowo. Dodano testy jednostkowe

---
+++ GraZaDuzoZaMalo/ClassLibrary1/Gra.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace ModelGry
+{
+    public class Gra
+    {
+        //innertypes
+        public enum Odpowiedz
+        {
+            ZaMalo = -1,
+            Trafiono = 0,
+            ZaDuzo = 1
+        }
+
+        public enum State
+        {
+            Trwa,
+            Poddana,
+            Odgadnieta
+        }
+
+
+        //fields
+        public State StanGry { get; private set; }
+        public readonly int ZakresOd;
+        public readonly int ZakresDo;
+        private readonly int wylosowana;
+        public int? Wylosowana
+        {
+            //jeżeli gra poddana lub zakończona, to
+            get
+            {
+                if (StanGry != State.Trwa)
+                    return wylosowana;
+                else
+                    return null;
+            }
+            //set { }
+        }
+        // historia gry: do zrobienia
+        public int LicznikRuchow { get; private set; } = 0;
+    
+        public Gra(int a, int b)
+        {
+            ZakresOd = Math.Min(a, b);
+            ZakresDo = Math.Max(a, b);
+            wylosowana = Losuj(ZakresOd, ZakresDo);
+            //LicznikRuchow =0;
+            StanGry = State.Trwa;
+        }
+
+        public Odpowiedz Ocena(int propozycja)
+        {
+            LicznikRuchow++;
+            if (propozycja < wylosowana)
+                return Odpowiedz.ZaMalo;
+            else if (propozycja > wylosowana)
+                return Odpowiedz.ZaDuzo;
+            else
+            {
+                StanGry = State.Odgadnieta;
+                return Odpowiedz.Trafiono;
+            }
+        } 
+        public void Poddaj()
+        {
+                StanGry = State.Poddana;
+        }
+
+        
+        public static int Losuj(int min = 1, int max = 100)
+        {
+            if (min > max)
+            {
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            Random generator = new Random();
+            return generator.Next(min, max + 1);
+        }
+    }
+}
